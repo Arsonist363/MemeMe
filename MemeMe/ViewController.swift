ae623c1ee213f7f69ed67ec34051dd14026f6a4e
Cesar Colorado <Cesar.Colorado@gmail.com>
2015-04-18 17:51:03 -0400

    Added photo album picker

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    
+    @IBOutlet weak var ImagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Image picker Controller
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.ImagePickerView.image = image
+        }
+        self.dismissViewControllerAnimated(false, completion: nil)
     }
-
-
+    
+    @IBAction func photoLibaryImagePicker(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        // Allow image to be cropped
+        imagePicker.allowsEditing = true
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
 }
 
