15d5ff4f50fc07097498adf1a73432bc7122fde0
Cesar Colorado <Cesar.Colorado@gmail.com>
2015-04-18 23:34:46 -0400

    added textfield
    textfield and textfield attributes

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -12,10 +12,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate{
     
     @IBOutlet weak var ImagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
+    // Custome Text Attributes
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 6
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //check if camera is availible
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
+        //change the tex fiels properties
+        topTextField.enabled = true
+        topTextField.textAlignment = .Center
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.enabled = true
+        bottomTextField.textAlignment = .Center
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
     }
     // Image picker Controller
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
@@ -31,6 +55,17 @@ UINavigationControllerDelegate{
         imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         // Allow image to be cropped
         imagePicker.allowsEditing = true
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraImagePicker(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        // Allow image to be cropped
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        imagePicker.cameraCaptureMode = .Photo
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
 }
