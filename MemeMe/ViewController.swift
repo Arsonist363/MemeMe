37c506dec050d26df9192858ce9780aa3da9cbda
Cesar Colorado <Cesar.Colorado@gmail.com>
2015-04-24 11:41:37 -0400

    Fixed Textfield

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,20 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate{
+UINavigationControllerDelegate, UITextFieldDelegate{
     
+    // Outlets
     @IBOutlet weak var ImagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
+    
+    
     
     // Custome Text Attributes
     let memeTextAttributes = [
         NSStrokeColorAttributeName : UIColor.whiteColor(),
         NSForegroundColorAttributeName : UIColor.whiteColor(),
         NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSStrokeWidthAttributeName : 6
+        NSStrokeWidthAttributeName : -3
     ]
+    
+    // Text Field Delegate objects
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,14 +40,25 @@ UINavigationControllerDelegate{
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
         
         //change the tex fiels properties
-        topTextField.enabled = true
+        
+        topTextField.text = "TOP"
         topTextField.textAlignment = .Center
         topTextField.defaultTextAttributes = memeTextAttributes
-        bottomTextField.enabled = true
+        topTextField.delegate = self
+        bottomTextField.text = "BOTTOM"
         bottomTextField.textAlignment = .Center
         bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.delegate = self
         
+        
+        // Subscribe to keyboard notifications to allow the view to raise when necessary
+        self.subscribeToKeyboardNotifications()
     }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotifications()
+    }
+    
     // Image picker Controller
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
@@ -75,5 +93,52 @@ UINavigationControllerDelegate{
         self.presentViewController(activityController, animated:true, completion:nil)
         
     }
+    func keyboardWillShow(notification: NSNotification) {
+        
+        self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(notification: NSNotification)
+    {
+
+        self.returnViewToInitialFrame()
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func returnViewToInitialFrame()
+    {
+        var initialViewRect: CGRect = CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height)
+        
+        if (!CGRectEqualToRect(initialViewRect, self.view.frame))
+        {
+            UIView.animateWithDuration(0.2, animations: {
+                self.view.frame = initialViewRect
+            });
+        }
+    }
+    
 }
 
---
+++ MemeMe/textFieldDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  textFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Cesar Colorado on 4/19/15.
+//  Copyright (c) 2015 Cesar Colorado. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class textFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    
+}
